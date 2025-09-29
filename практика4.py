817d54fb28a566e6595217591c0253746ada3f13
glebkasimov19-ai <glebkasimov19@gmail.com>
2025-09-29 09:43:45 +0300

    практика4.py

---
+++ практика4.py
@@ -0,0 +1,43 @@
+# Задачи
+#1.
+A = int(input("Введите число A:"))
+B = int(input("Введите число B:"))
+if A <= B:
+    while A <= B:
+        print(A)
+        A = A + 1
+else:
+    print("Ошибка: А должно быть меньше или равно B")
+
+
+#2.
+A = int(input("Введите число A:"))
+B = int(input("Введите число B:"))
+if A < B:
+    for i in range(A, B + 1):
+        print(i)
+else:
+    for i in range(A, B - 1, -1):
+        print(i)
+
+
+#3.
+A = int(input("Введите число A:"))
+B = int(input("Введите число B:"))
+
+for i in range(A, B - 1, -1):
+    if i % 2 != 0:
+        print(i)
+
+
+#4.
+N = int(input("Введите число N:"))
+print(sum(int(input())for _ in range(N)))
+
+
+#5.
+n = int(input("Введите число n:"))
+s = 0
+for i in range(1, n + 1):
+    s = s + i ** 3
+print(s)
