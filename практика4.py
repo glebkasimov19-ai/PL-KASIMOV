7746009c935b48178ec4598c8139bbe224668879
glebkasimov19-ai <glebkasimov19@gmail.com>
2025-10-27 12:25:02 +0300

    Update практика4.py

--- практика4.py
+++ практика4.py
@@ -47,11 +47,11 @@ print(s)
 #7.
 n = int(input("n = "))
 
-sum = 0       # здесь будем накапливать сумму факториалов
-faсtorial = 1   # здесь будем хранить текущий факториал (начинаем с 1!)
+sum = 0       
+faсtorial = 1  
 
 for i in range(1, n + 1):
-    faсtorial = faсtorial * i   # например: 2! = 1! * 2 → 1*2=2
-    sum = sum + faсtorial   # добавляем к общей сумме
+    faсtorial = faсtorial * i  
+    sum = sum + faсtorial   
 
 print("Ответ:", sum)
