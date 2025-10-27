539f39455290c5b8caa0d56f45602d1ce2f73aa7
glebkasimov19-ai <glebkasimov19@gmail.com>
2025-10-27 11:42:10 +0300

    Update практика6.py

--- практика6.py
+++ практика6.py
@@ -1,27 +1,25 @@
 # Вариант 7
 
 # Задача 7.1
-N = int(input("Введите количество элементов: "))
-x = []
-for i in range(N):
-    x.append(int(input("x[" + str(i) + "] = ")))
+n = int(input("Введите количество элементов: "))
+a = []
+for i in range(n):
+    a.append(int(input("a[" + str(i) + "] = ")))
 sum_even = 0
 prod_odd = 1
-for i in range(N):
+for i in range(n):
     if i % 2 == 0:                                     
-        sum_even += x[i]
+        sum_even += a[i]
     else:                                               
-        prod_odd *= x[i]
+        prod_odd *= a[i]
 print("Сумма элементов с четными номерами:", sum_even)
 print("Произведение элементов с нечетными номерами:", prod_odd)
 
 # Задача 7.2
-min_i = 0
-max_i = 0
-for i in range(1, N):                                    
-    if x[i] < x[min_i]:
-        min_i = i
-    if x[i] > x[max_i]:
-        max_i = i
-x[min_i], x[max_i] = x[max_i], x[min_i]                 
-print("Массив после обмена min и max:", x)
+min_i = a.index(min(a))
+max_i = a.index(max(a))
+
+a[min_i], a[max_i] = a[max_i], a[min_i]
+
+print("После обмена:", a)
+
