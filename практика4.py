a256d9d4afb1591fe7a6cdb93dc9fb914e453aac
glebkasimov19-ai <glebkasimov19@gmail.com>
2025-10-27 11:59:27 +0300

    Update.практика4.py

--- практика4.py
+++ практика4.py
@@ -41,3 +41,15 @@ s = 0
 for i in range(1, n + 1):
     s = s + i ** 3
 print(s)
+
+#7.
+n = int(input("n = "))
+
+sum = 0       # здесь будем накапливать сумму факториалов
+faсtorial = 1   # здесь будем хранить текущий факториал (начинаем с 1!)
+
+for i in range(1, n + 1):
+    faсtorial = faсtorial * i   # например: 2! = 1! * 2 → 1*2=2
+    sum = sum + faсtorial   # добавляем к общей сумме
+
+print("Ответ:", sum)
---
+++ практика5.py
@@ -0,0 +1,94 @@
+#Вариант 1
+print("Введите строку:")
+s = input()
+
+words = s.split(' ')  
+count = 0
+
+for i in range(len(words)): 
+    word = words[i]
+    if len(word) > 0 and (word[0] == 'е' or word[0] == 'Е'): 
+        count = count + 1  
+
+print("Количество слов, начинающихся с 'е':", count)
+
+
+
+#Вариант 2
+print("Введите строку:")
+s = input()
+
+count = s.count(':')        
+new_s = s.replace(':', '%') 
+
+print("Новая строка:", new_s)
+print("Количество замен:", count)
+
+
+
+
+#Вариант 3
+print("Введите строку:")
+s = input()
+
+count = s.count('.')       
+new_s = s.replace('.', '') 
+print("Новая строка:", new_s)
+print("Удалено символов '.':", count)
+
+
+
+
+
+
+#Вариант 4
+print("Введите строку:")
+s = input()
+
+count = s.count('а')       
+new_s = s.replace('а', 'о')
+length = len(new_s)        
+
+print("Новая строка:", new_s)
+print("Количество замен:", count)
+print("Длина строки:", length)
+
+
+
+
+#Вариант 5
+s = input("Введите строку: ")
+print("Результат:", s.lower())
+
+
+
+
+
+
+
+#Вариант 6
+print("Введите строку:")
+s = input()
+
+count = s.count('a')       
+new_s = s.replace('a', '') 
+
+print("Новая строка:", new_s)
+print("Удалено символов 'a':", count)
+
+
+
+
+
+
+#Вариант 7
+print("Введите строку:")
+s = input()
+
+n = len(s)
+half = s[0:n//2]          
+
+if 'n' in half:
+    s = s.replace('n', '*')         
+
+print("Результат:", s)
