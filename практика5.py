dcd7ca2d69f541d61f07e676dcdd35635b78c665
glebkasimov19-ai <glebkasimov19@gmail.com>
2025-10-27 11:23:43 +0300

    Update практика5.py

--- практика5.py
+++ практика5.py
@@ -1,16 +1,6 @@
 #Вариант 5
-print("Введите строку:")
-s = input()
-
-result = ''
-for i in range(len(s)):
-    ch = s[i]
-    if 'А' <= ch <= 'Я':          
-        ch = chr(ord(ch) + 32)    
-    result = result + ch
-
-print("Результат:", result)
-
+s = input("Введите строку: ")
+print("Результат:", s.lower())
 
 
 #Вариант 7
@@ -19,11 +9,8 @@ s = input()
 
 n = len(s)
 half = s[0:n//2]          
-found = half.find('n')     
 
-if found != -1:           
-    new_s = s.replace('n', '*')  
-else:
-    new_s = s              
+if 'n' in half:
+    s = s.replace('n', '*')         
 
-print("Результат:", new_s)
+print("Результат:", s)
