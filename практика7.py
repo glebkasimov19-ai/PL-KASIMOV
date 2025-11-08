e02703c17bc73d8a9ab1cb3298365b7cbdee5da4
glebkasimov19-ai <glebkasimov19@gmail.com>
2025-11-09 00:54:19 +0300

    Creat практика7.py

--- практика7.py
+++ практика7.py
@@ -1,11 +1,59 @@
-#7.
-n = int(input("n = "))
+# Варинат 5
+# Задача 5.1
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+A = int(input("Введите A: "))
+B = int(input("Введите B: "))
+C = int(input("Введите C: "))
+D = int(input("Введите D: "))
 
-sum = 0       # здесь будем накапливать сумму факториалов
-faсtorial = 1   # здесь будем хранить текущий факториал (начинаем с 1!)
+numerator = A * D - C * B
+denominator = B * D
+
+if denominator < 0:
+    numerator = -numerator
+    denominator = -denominator 
+
+g = gcd(abs(numerator), abs(denominator))
+numerator //= g
+denominator //= g
+
+print(f"Результат: {numerator}/{denominator}")
+
+
+#Задача 5.2
+n = int(input("Введите число: "))
+deliteli = []
 
 for i in range(1, n + 1):
-    faсtorial = faсtorial * i   # например: 2! = 1! * 2 → 1*2=2
-    sum = sum + faсtorial   # добавляем к общей сумме
+    if n % i == 0:
+        deliteli.append(str(i))  
+print(" ".join(deliteli))
 
-print("Ответ:", sum)
+
+
+# Вариант 7
+# Задача 7.1
+def area_rectangle(a, b):
+    return a * b
+
+def area_right_triangle(a, b):
+    return (a * b) / 2
+
+X = int(input("Введите длину стороны X: "))
+Y = int(input("Введите длину стороны Y: "))
+Z = int(input("Введите длину стороны Z: "))
+T = int(input("Введите длину стороны T: "))
+
+print("Фигура — прямоугольник.")
+area = area_rectangle(X, Y)
+print(f"Площадь прямоугольника: {area}")
+
+
+
+# Задача 7.2
+n = int(input("Введите неотрицательное целое число:"))
+s = f"{n:o}"
+print('0' * (10 - len(s)) + s)
