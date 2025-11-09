037ed580039da97d20186d3c1973d9bb0c602c9c
glebkasimov19-ai <glebkasimov19@gmail.com>
2025-11-09 22:54:43 +0300

    Update практика9.py

--- практика9.py
+++ практика9.py
@@ -7,6 +7,21 @@ def print_digits_reverse(n):
         print(n % 10)
         print_digits_reverse(n // 10)
 
-# Пример использования
 N = int(input("Введите натуральное число N: "))
 print_digits_reverse(N)
+
+# Блок Б
+# Задача 3
+def f():
+    if not hasattr(f, 'arr'):
+        f.arr = []
+    n = int(input())
+    if n == 0:
+        print("Результат:")
+        for i in range(0, len(f.arr), 2):
+            print(f.arr[i])
+        f.arr = []
+        return
+    f.arr.append(n)
+    f()
+f()
